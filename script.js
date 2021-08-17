35ac2ee4c66d0b8a8fe415957d3029e4757242e5
MikeAdu <mike_adusah@yahoo.com>
2021-08-16 22:15:24 -0700

    added new if statment

--- script.js
+++ script.js
@@ -6,13 +6,41 @@ var upperCase = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
 var numbers = ["0123456789"];
 var specialChars = ["!@#$%^&*()_=+"];
 
-
+function generatePassword () {
 var passwordLength = window.prompt("Password must be between 8-28 characters");
 var isUpperCase = window.confirm("Will there be upper case letters in the password?");
 var isLowerCase = window.confirm("Will there be lower case letters in the password?");
 var hasSpecialCharacters= window.confirm("Will the password have special characters?");
 var hasNumbers = window.confirm("Will the password have numbers?");
 
+
+
+var successfulPassword =[];
+var finalPassword = [];
+
+if (isLowerCase) {
+  successfulPassword = successfulPassword.concat(lowerCase);
+}
+
+if (isUpperCase) {
+  successfulPassword = successfulPassword.concat(upperCase);
+}
+
+if (hasNumbers) {
+  successfulPassword = successfulPassword.concat(numbers);
+}
+
+if (hasSpecialCharacters) {
+  successfulPassword = successfulPassword.concat(specialChars);
+}
+
+for (var i = 0; i < passwordLength; i++) {
+  user.push(successfulPassword[Math.floor(Math.random() * successfulPassword.length)]);
+}
+
+return finalPassword.join("");
+
+}
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();

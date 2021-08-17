d4339538dde3f38653af460698f085bf86d6c318
MikeAdu <mike_adusah@yahoo.com>
2021-08-16 18:44:53 -0700

    added window.prompt and window.confirm to script.js

--- script.js
+++ script.js
@@ -4,6 +4,13 @@ var lowerCase = "abcdefghijklmnopqrstuvwxyz";
 var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var numbers = "0123456789"
 var specialChars = "!@#$%^&*()_=+"
+
+
+var passwordLength = window.prompt("Password must be between 8-28 characters");
+var isUpperCase = window.confirm("Will there be upper case letters in the password?");
+var isLowerCase = window.confirm("Will there be lower case letters in the password?");
+var hasSpecialCharacters= window.confirm("Will the password have special characters?");
+var hasNumbers = window.confirm("Will the password have numbers?");
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();

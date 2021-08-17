117cd2fe29792ca637a9deb62e186bfc5384721e
MikeAdu <mike_adusah@yahoo.com>
2021-08-16 22:59:52 -0700

    adding if statments to make password

--- script.js
+++ script.js
@@ -6,41 +6,47 @@ var upperCase = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
 var numbers = ["0123456789"];
 var specialChars = ["!@#$%^&*()_=+"];
 
-function generatePassword () {
-var passwordLength = window.prompt("Password must be between 8-28 characters");
-var isUpperCase = window.confirm("Will there be upper case letters in the password?");
-var isLowerCase = window.confirm("Will there be lower case letters in the password?");
-var hasSpecialCharacters= window.confirm("Will the password have special characters?");
-var hasNumbers = window.confirm("Will the password have numbers?");
+function generatePassword() {
+  var successfulPassword = [];
+  var finalPassword = "";
+  if (passwordLength < 8 || passwordLength >= 28) {
+    window.alert("Password must be between 8-28 characters");
+  }
 
+  var passwordLength = window.prompt("Password must be between 8-28 characters");
+  var isUpperCase = window.confirm("Will there be upper case letters in the password?");
+  var isLowerCase = window.confirm("Will there be lower case letters in the password?");
+  var hasSpecialCharacters = window.confirm("Will the password have special characters?");
+  var hasNumbers = window.confirm("Will the password have numbers?");
+  for (i = 0; i < passwordLength; i++) {
+    finalPassword.push(successfulPassword[Math.floor(Math.random() * successfulPassword.length)]);
+    if (hasSpecialCharacters === true) {
+      successfulPassword.concat(specialChars)
 
+    }
+    if (isLowerCase === true) {
+      successfulPassword.concat(lowerCase)
+    }
+    if (isUpperCase === true) {
+      successfulPassword.concat(upperCase)
 
-var successfulPassword =[];
-var finalPassword = [];
+    }
+    if (hasNumbers === true) {
+      successfulPassword.concat(numbers)
+    }
 
-if (isLowerCase) {
-  successfulPassword = successfulPassword.concat(lowerCase);
+  }
+  return finalPassword;
 }
 
-if (isUpperCase) {
-  successfulPassword = successfulPassword.concat(upperCase);
-}
 
-if (hasNumbers) {
-  successfulPassword = successfulPassword.concat(numbers);
-}
 
-if (hasSpecialCharacters) {
-  successfulPassword = successfulPassword.concat(specialChars);
-}
 
-for (var i = 0; i < passwordLength; i++) {
-  user.push(successfulPassword[Math.floor(Math.random() * successfulPassword.length)]);
-}
 
-return finalPassword.join("");
 
-}
+
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
